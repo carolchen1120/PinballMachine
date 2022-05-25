c2c879bcb49da2ead3bc66b2df7aebaeba413cdd
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2022-05-25 08:31:25 -0400

    Update pinball.py

--- pinball.py
+++ pinball.py
@@ -2,5 +2,14 @@ import turtle as t
 
 window = t.Screen()
 window.bgcolor("white")
+
 ball = t.Turtle()
+ball.shape("circle")
+ball.penup()
 ball.goto(150,0)
+ball.done()
+
+wall = t.Turtle()
+wall.penup()
+wall.goto(170, -20)
+wall.done()
