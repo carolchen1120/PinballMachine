cab161478d28fefd3cda8812b3568737c47606ae
ewizy <106249729+ewizy@users.noreply.github.com>
2022-05-25 15:15:35 -0400

    Update pinball.py

--- pinball.py
+++ pinball.py
@@ -44,11 +44,37 @@ wall.goto(220, -200)
 wall.pendown()
 wall.goto(130, -290)
 
-#flippers
+#flipper1
 f1 = t.Turtle()
 f1.shape("triangle")
 f1.shapesize(1, 4, 1)
 f1.penup()
+f1.goto(-60, -240)
+f1.tilt(345)
+
+#flipper2
+f2 = t.Turtle()
+f2.shape("triangle")
+f2.shapesize(1, 4, 1)
+f2.penup()
+f2.goto(60, -240)
+f2.tilt(195)
+
+#flipper walls
+fwall1 = t.Turtle()
+fwall1.shape("square")
+fwall1.shapesize(1, 4, 1)
+fwall1.penup()
+fwall1.goto(-120 ,-200)
+fwall1.tilt(315)
+
+fwall2 = t.Turtle()
+fwall2.shape("square")
+fwall2.shapesize(1, 4, 1)
+fwall2.penup()
+fwall2.goto(120, -200)
+
 
 
 t.done()
+
